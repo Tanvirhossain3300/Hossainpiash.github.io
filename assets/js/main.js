3300dd9e2794bd7e5674dc8614d30c273301633c
tanvirhossain3300 <tanvirhossain3300@github.com>
2018-03-04 01:35:44 +0600

    My first website
    This is very helpful,

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            nav: false,
+            dots: true
+        });
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
